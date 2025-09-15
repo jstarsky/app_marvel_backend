ccd905d01a644788ec21a012bfe3bea3d9d764f8
Jose Starsky Naya Lara <starsky@Joses-MBP.home>
2025-09-15 10:27:09 +0200

    wip

--- app_marvel_backend/urls.py
+++ app_marvel_backend/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('apps.authentication.urls')),
+    # Provide legacy /auth/ endpoints for deploy/platform compatibility
+    path('auth/', include('apps.authentication.urls')),
 ]
